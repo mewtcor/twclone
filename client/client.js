520ef06b0fd7bd71738f563840d4d3fa36db6e7f
mewtcor <llcorpuz@gmail.com>
2020-08-18 13:48:16 +1200

    day 2

--- client/client.js
+++ client/client.js
@@ -1,8 +1,32 @@
 console.log('Hello World!');
 
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/mews';
+
+loadingElement.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('form was submitted');
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  // grab contents
+  const mew = {
+    name,
+    content
+  };
+  console.log(mew)
+  form.style.display = 'none'; // hide form
+  loadingElement.style.display = ''; // display loading gif
+
+  // send data(json) to the server
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(mew),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
 });
